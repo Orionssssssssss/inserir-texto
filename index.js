7965b3f6d6bc9dd97294a6a1a73fc5544c4755a1
Orionssssssssss <145810678+Orionssssssssss@users.noreply.github.com>
2026-09-19 10:06:08 -0300

    Update bot host and maintain server functionality
    Updated bot host to 'inserirtexto-gWHK.aternos.me' and preserved server functionality.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.listen(PORT, () => {
 // Código do Bot
 function createBot() {
   const bot = mineflayer.createBot({
-    host: 'mosquitofish.aternos.host',
+    host: 'inserirtexto-gWHK.aternos.me',
     port: 38060,
     username: 'inserirtexto',
     // Define a versão exata do Minecraft (ex: '1.20.4' ou '1.20.6' dependendo do teu servidor)

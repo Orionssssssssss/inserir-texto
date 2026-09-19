547ed75f7b675455b26fdeba691df33aa524243c
Orionssssssssss <145810678+Orionssssssssss@users.noreply.github.com>
2026-09-19 10:25:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mineflayer = require('mineflayer')
 // ==========================================
 const CONFIG = {
   // Endereço do servidor (pode ser o IP normal ou o DynIP)
-  host: 'inserirtexto-gWHK.aternos.me',
+  host: 'penguin.aternos.host',
   
   // Porta do servidor (38060 indicada na Aternos)
   port: 38060,
